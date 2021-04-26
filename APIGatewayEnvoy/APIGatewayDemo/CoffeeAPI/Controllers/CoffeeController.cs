0d6f2eb4836c5c7eceb930cd11bcb5c77daf2ab0
Najim <n.doudouh@student.fontys.nl>
2021-04-26 14:55:30 +0200

    APIGateway

---
+++ APIGatewayEnvoy/APIGatewayDemo/CoffeeAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        private static string[] CoffeeTypes = new[]
+        {
+            "Flat White", "Long Black","Latte","Americano","Cappuccino",
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            Random rnd = new Random();
+            return Ok(CoffeeTypes[rnd.Next(CoffeeTypes.Length)]);
+        }
+    }
+}
---
+++ APIGatewayEnvoy/APIGatewayDemo/TeaAPI/Controllers/TeaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeaAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TeaController : ControllerBase
+    {
+        private static string[] TeaTypes = new[]
+        {
+            "Aardbeien", "Bosvruchten","Citroen","Ne3ne3","Shiba",
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            Random rnd = new Random();
+            return Ok(TeaTypes[rnd.Next(TeaTypes.Length)]);
+        }
+    }
+}
